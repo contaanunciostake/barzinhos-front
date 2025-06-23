2dcfac8821e98d54d01b1873438bc3bf881e321a
contaanunciostake <contaanunciostake@gmail.com>
2025-06-22 22:25:57 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 
 export default defineConfig({
-  base: './', // 👈 ISSO É ESSENCIAL PARA FUNCIONAR NO VERCEL!
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
